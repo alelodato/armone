5ac8a882d981f0015c9e552137c25447f59f2fdb
alelodato <alss.ldt@gmail.com>
2025-11-10 16:16:38 +0100

    created classi and contact pages and added routes to app

--- armonestudio/src/App.jsx
+++ armonestudio/src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Classi from "./pages/Classi";
+import Contact from "./pages/Contact";
 
 export default function App() {
   return (
@@ -11,6 +13,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/classi" element={<Classi />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </>
